77e68f51b3db7c3f92d6a129462fe2117d9f65c2
Daniel Olsson <fipaso@gmail.com>
2015-03-25 20:53:02 +0100

    just noticed there was an example on the course webpage. oh well, it actually works

--- app/src/main/java/com/dabbe/compass/CompassView.java
+++ app/src/main/java/com/dabbe/compass/CompassView.java
@@ -14,15 +14,15 @@ import android.view.SurfaceView;
 public class CompassView extends SurfaceView implements SurfaceHolder.Callback {
 
     private static final String[] cardinal = {"North", "Northeast", "East", "Southeast", "South", "Southwest", "West", "Northwest"};
-
+    private static final String[] cardinalShort = {"N", "E", "S", "W"};
     private Path topPath, botPath;
     private SurfaceHolder holder;
     private DrawingThread thread;
-    private Paint paint, topPathPaint, botPathPaint, middlePaint;
+    private Paint paint, topPathPaint, botPathPaint, middlePaint, backgroundPaint, linePaint;
 
     private Compass compass;
-    private float width, height, arrowHeight, arrowWidth;
-    ;
+    private float width, height, arrowHeight, arrowWidth, radius;
+
 
     public CompassView(Context context, Compass compass) {
         super(context);
@@ -37,8 +37,14 @@ public class CompassView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     private void initGraphical() {
+        width = getWidth();
+        height = getHeight();
+        arrowWidth = width / 15f;
+        arrowHeight = width / 2.3f;
+        radius = width / 2.1f;
+
         paint = new Paint();
-        paint.setTextSize(50);
+        paint.setTextSize(150);
         paint.setColor(Color.RED);
 
         topPathPaint = new Paint();
@@ -52,11 +58,17 @@ public class CompassView extends SurfaceView implements SurfaceHolder.Callback {
         middlePaint = new Paint();
         middlePaint.setColor(Color.BLACK);
         middlePaint.setStyle(Paint.Style.FILL);
+        middlePaint.setStrokeWidth(width * 0.008f);
+        middlePaint.setTextSize(120);
 
-        width = getWidth();
-        height = getHeight();
-        arrowWidth = width / 15f;
-        arrowHeight = height / 3f;
+        linePaint = new Paint();
+        linePaint.setColor(Color.BLACK);
+        linePaint.setStrokeWidth(width * 0.015f);
+
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(Color.LTGRAY);
+        backgroundPaint.setStyle(Paint.Style.FILL);
+
 
         topPath = new Path();
         topPath.moveTo(width / 2f, height / 2f - arrowHeight);
@@ -102,11 +114,27 @@ public class CompassView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     public void drawCompass(Canvas canvas) {
+
+        canvas.save();
         float deg = compass.getDegrees();
         canvas.drawColor(Color.BLACK);
+        canvas.drawCircle(width / 2f, height / 2f, radius, backgroundPaint);
+        canvas.rotate(deg, width/2f, height/2f);
+        for (int i = 0; i < 4; i++) {
+            canvas.drawLine(width / 2f, height / 2f + radius, width / 2f, height / 2f + radius - height * 0.05f, linePaint);
+            canvas.drawText(cardinalShort[i], width / 2f - middlePaint.measureText(cardinalShort[i]) / 2, height / 2f + radius - height * 0.08f, middlePaint);
+            canvas.rotate(360 / 4, width / 2f, height / 2f);
+        }
+        for (int i = 0; i < 32; i++) {
+            canvas.rotate(360 / 32f, width / 2f, height / 2f);
+            canvas.drawLine(width / 2f, height / 2f + radius, width / 2f, height / 2f + radius - height * 0.03f, middlePaint);
+        }
+        canvas.restore();
+
         canvas.drawPath(topPath, topPathPaint);
         canvas.drawPath(botPath, botPathPaint);
-        canvas.drawCircle(width / 2f, height / 2f, width / 20f, middlePaint);
-        canvas.drawText(cardinal[Math.round((deg / 360.0f) * (cardinal.length - 1))], 500, 500, paint);
+        canvas.drawCircle(width / 2f, height / 2f, width / 25f, middlePaint);
+        canvas.drawText(deg + " degrees", 500, 500, paint);
+
     }
 }
